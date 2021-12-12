b0386d8220df66ccfdea1183721042a98d5a4c4e
Muss86 <florin.gruianu@gmail.com>
2021-12-12 04:06:27 +0200

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,42 @@
 import './../scss/index.scss';
+import Glide from '@glidejs/glide';
+
+new Glide('.product-tile-list.glide', {
+  type: 'carousel',
+  perView: 4,
+  breakpoints: {
+    991: {
+      perView: 2,
+    },
+  },
+}).mount();
+
+new Glide('.home-catalog-preview:nth-of-type(2) .product-tile-list.glide', {
+  type: 'carousel',
+  perView: 4,
+  breakpoints: {
+    991: {
+      perView: 2,
+    },
+  },
+}).mount();
+
+new Glide('.home-catalog-preview:nth-of-type(4) .product-tile-list.glide', {
+  type: 'carousel',
+  perView: 4,
+  breakpoints: {
+    991: {
+      perView: 2,
+    },
+  },
+}).mount();
+
+new Glide('.home-catalog-preview:nth-of-type(5) .product-tile-list.glide', {
+  type: 'carousel',
+  perView: 4,
+  breakpoints: {
+    991: {
+      perView: 2,
+    },
+  },
+}).mount();
